c138602ff7828c7a112ea194b0d8a95d9c103a42
csap0422 <Christopher543sap@gmail.com>
2018-11-07 12:46:46 -0600

    Initial

--- src/com/cfs/intro/Main.java
+++ src/com/cfs/intro/Main.java
@@ -190,6 +190,17 @@ public class Main {
      * @throws RuntimeException if columns of a does not equal rows of b
      */
     public static double[][] dot(double[][] a, double[][] b){
-        if()
+        if(a[0].length != b.length){
+            throw new RuntimeException("Illegal matrix dimensions");
+        }
+        double[][] c = new double[a.length][a[0].length];
+        for(int i = 0; i < a.length; i++){
+            for(int j = 0; j < b[0].length; j++){
+                for(int k = 0; k < a[0].length; k++){
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return c;
     }
 }
